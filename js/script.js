e85d2d2f5c51d6898e15a3352ae283a6e84e4d6f
Matias Skødt <matiasskodt@gmail.com>
2019-03-19 13:17:14 +0100

    pop up vindue

--- js/script.js
+++ js/script.js
@@ -128,3 +128,36 @@ console.log(elements);
 $(document).ready(function() {
   $("input[type=password]").mobilePassword();
 });
+
+
+// POP UP NUDGING
+
+// Get the modal
+let modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function openModal() {
+  let modal = document.getElementById('myModal');
+  modal.style.display = "block";
+  console.log(123);
+}
+
+// When the user clicks on <span> (x), close the modal
+function close() {
+  let modal = document.getElementById('myModal');
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  let modal = document.getElementById('myModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
